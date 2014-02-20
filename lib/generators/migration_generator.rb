a629e17879bf7ef72d8d15ac765142ec9ed2792d
Michal Pisanko <michal@hooroo.com>
2014-02-20 15:36:42 +1100

    initial generator for migrations

---
+++ lib/generators/migration_generator.rb
@@ -0,0 +1,9 @@
+#encoding: utf-8
+
+class MigrationGenerator < Rails::Generators::Base
+
+  desc "This generator creates a migration for persisting feature toggles via Active Record"
+  def generate_migration
+
+  end
+end
