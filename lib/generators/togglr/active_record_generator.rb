2bd18d3993d8cb77ab264d613bc691c3eb4d2d0b
Brett Wilkins <brett@brett.geek.nz>
2014-02-21 15:20:41 +1100

    Correcting namespace conflict T_T

--- lib/generators/togglr/active_record_generator.rb
+++ lib/generators/togglr/active_record_generator.rb
@@ -4,7 +4,7 @@ require 'rails/generators'
 require 'rails/generators/active_record'
 
 module Togglr
-  class ActiveRecordGenerator < ActiveRecord::Generators::Base
+  class ActiveRecordGenerator < ::ActiveRecord::Generators::Base
     # Inspired by the friendly_id gem. Since ActiveRecord::Generators::Base inherits from Rails::Generators::NamedBase,
     # this class requires a NAME parameter. We always use 'togglr_toggles', so we set a random name as the default here.
     argument :name, type: :string, default: 'random_name'
