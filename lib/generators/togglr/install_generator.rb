5d2c60c920e6c7865c247908812e1301d9188826
Brett Wilkins <brett@brett.geek.nz>
2014-02-21 11:38:46 +1100

    Adding generator for the default initializer

---
+++ lib/generators/togglr/install_generator.rb
@@ -0,0 +1,14 @@
+#encoding: utf-8
+
+require 'rails/generators'
+
+module Togglr
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('./templates', File.dirname(__FILE__))
+
+    desc "This generator creates an intializer for Togglr"
+    def generate_initializer
+      template('initializer.rb', 'config/initializers/togglr.rb')
+    end
+  end
+end
---
+++ lib/generators/togglr/templates/initializer.rb
@@ -0,0 +1,11 @@
+#encoding: utf-8
+
+require 'togglr'
+require 'togglr/toggles'
+
+Togglr.configure do |config|
+  config.toggles_file = File.expand_path(File.join(Rails.root, 'config', 'togglr.yml'))
+  config.repositories = []
+end
+
+Togglr::Toggles.register_toggles
